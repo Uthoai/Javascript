868d55d546a11f2a5df07b791b763f1fefcbfa8d
Uthoai Marma <134888799+Uthoai@users.noreply.github.com>
2024-12-29 11:50:56 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,37 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3+3) // we are using nodeJs, not browser
+
+console.log(3+
+    3); // code readability should be high
+
+console.log("Uthoai Marma");
+
+
+//Data Types
+
+let name = "Uthoai";
+let age = 26;
+let isLogIn = true;
+let state = null
+
+// number => 2 to power 53
+// bigint
+// string => "" or ''
+// boolean => true/false
+// null => standalone value / empty value
+// undefined => no value asign
+
+// symbol => unique
+
+
+//object
+
+
+//display which type
+console.log(typeof null);
+console.log(typeof undefined);
+
+
+
+
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,12 @@
+let score = "33"
+
+console.log(typeof score);
+
+// convert sting to number
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+score = "33abe"
+valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
