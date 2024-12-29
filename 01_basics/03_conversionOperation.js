66c1b714feae8c5145328689223d6bc4a1d8c63c
Uthoai Marma <134888799+Uthoai@users.noreply.github.com>
2024-12-29 15:38:39 +0000

    conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,12 +1,41 @@
 let score = "33"
-
 console.log(typeof score);
+
 
 // convert sting to number
 let valueInNumber = Number(score)
 console.log(typeof valueInNumber);
 
-score = "33abe"
+
+score = "33abc"
 valueInNumber = Number(score)
 console.log(typeof valueInNumber);
 console.log(valueInNumber);
+
+if (isNaN(valueInNumber)) {
+    console.log("wrong number");
+}else {
+    console.log("This is Number " + valueInNumber);
+}
+// conversion other type to number
+// "33" => 33
+// "33abc" => NaN
+// true => 1 / false = 0
+// null => 0
+
+
+// conversion number to boolean
+let isLogin = "login done"
+let numberLogin = Boolean(isLogin)
+
+console.log(numberLogin);
+// 1=> true; 0=> false
+// "string"=> true; ""=> false
+
+
+// number to string
+let number = 33
+let numberToString = String(number);
+
+console.log(numberToString);
+
