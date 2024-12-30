46d0f5da505239633c22b95fbbb5c6607b822b80
Uthoai Marma <134888799+Uthoai@users.noreply.github.com>
2024-12-30 08:56:56 +0000

    num and math

---
+++ 01_basics/06_num_and_math.js
@@ -0,0 +1,54 @@
+const score = 400
+
+let balance = new Number(125)
+// console.log(balance.toString().length);     //3
+// console.log(balance.toFixed(2));            //125.00
+
+let otherNumber = 520.856398
+// console.log(otherNumber.toPrecision(6));    //520.856   return type is string
+// console.log(otherNumber.toPrecision(5));    //520.86
+// console.log(otherNumber.toPrecision(4));    //520.9
+
+// console.log(otherNumber.toPrecision(3));    //521
+// otherNumber = 520.256
+// console.log(otherNumber.toPrecision(3));    //520
+
+// otherNumber = 10520.256
+// console.log(otherNumber.toPrecision(3));    //1.05e+4
+
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString());            //10,00,000
+// console.log(hundreds.toLocaleString('en-IN'));     //1,000,000
+
+
+// console.log(Number.MAX_SAFE_INTEGER);       //9007199254740991
+
+
+// ++++++++++++++++ Maths ++++++++++++++++++++++
+
+console.log(Math);
+// console.log(Math.abs(-4));      // negative to positive
+
+// console.log(Math.round(4.3));   // 4
+// console.log(Math.round(4.5));   // 5
+// console.log(Math.round(4.8));   // 5
+
+// console.log(Math.ceil(5.2));    // 6
+// console.log(Math.floor(4.9));   // 4
+
+// console.log(Math.sqrt(5));      //2.23606797749979
+// console.log(Math.sqrt(5).toFixed(2));  //2.24
+
+// console.log(Math.max(156,452,56))      //452
+
+
+// console.log(Math.random()*10 + 1);     
+// console.log(Math.random()*10 + 1);
+
+// const min = 10
+// const max = 20
+// console.log(Math.floor(Math.random() * (max - min + 1) + min));
+
+
+
