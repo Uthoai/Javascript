ac26483de732992143ed41e07cef8e61badb8a8d
Uthoai Marma <134888799+Uthoai@users.noreply.github.com>
2025-01-11 06:47:04 +0000

    "control flow"

---
+++ 03_bascis/04_control_flow.js
@@ -0,0 +1,15 @@
+
+// let score = 200
+
+// if (typeof(score) === "number") {
+//     console.log("Execute");
+// }
+
+
+let  balance = 1000
+// Avoid this-
+// if (balance > 1500) console.log("Can withdraw"), console.log("can't withdraw");
+
+
+// similer syntex like other language and same concept
+
