2b5f23950e58590e6bfdd2e2142c7739a068b84d
Uthoai Marma <134888799+Uthoai@users.noreply.github.com>
2025-01-11 05:26:44 +0000

    "IIFE"

---
+++ 03_bascis/03_iife.js
@@ -0,0 +1,16 @@
+// Immediately Invoked Fuction Expression
+
+(function chai(){
+    console.log("DB CONNECTED");
+})();
+
+(function aurCode(){
+    console.log("DB CONNECTED");
+})();
+
+console.log("Hello");
+
+(()=>{
+    console.log("DB CONNECTED");
+})()
+
