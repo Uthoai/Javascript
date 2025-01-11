330aded8c9eda17dd289c2669d66ab33ff58849d
Uthoai Marma <134888799+Uthoai@users.noreply.github.com>
2025-01-11 07:53:58 +0000

    "control flow"

---
+++ 04_control_flow/01_control_flow.js
@@ -0,0 +1,39 @@
+
+// let score = 200
+
+// if (typeof(score) === "number") {
+//     console.log("Execute");
+// }
+
+
+let  balance = 1000
+// Avoid this-
+// if (balance > 1500) console.log("Can withdraw"), console.log("can't withdraw");
+
+
+// similer syntex like other language and same concept
+
+
+
+
+// let day = 5
+// switch (day) {
+//     case 1:
+//         console.log("Saturday");
+//         break;
+//     case 2:
+//         console.log("Sunday");
+//         break;
+//     case 3:
+//         console.log("Monday");
+//         break;
+//     case 4:
+//         console.log("Tuesday");
+//         break;
+    
+//     default:
+//         console.log("Wrong");
+//         break;
+// }
+
+
---
+++ 04_control_flow/02_truthy.js
@@ -0,0 +1,34 @@
+let userEmail = "uthoaimarma597@gmail.com"
+
+// if (userEmail) {
+//     console.log("Got user email");
+// } else {
+//     console.log("Don't have user email");
+// }
+
+userEmail = ""
+// if (userEmail) {
+//     console.log("Got user email");
+// } else {
+//     console.log("Don't have user email");
+// }
+
+// falsy values
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+
+// if string is not empty is true like- "0", "false"," "
+// [], {}, function(){} thoes are ture
+
+
+
+// let arr = []
+// if(arr.length === 0){
+//     console.log("Array is empty")
+// }
+
+// let empObj = {}
+// if (Object.keys(empObj).length === 0) {
+//     console.log("Object is Empty")
+// }
+
