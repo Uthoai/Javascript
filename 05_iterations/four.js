c7d39059ff81bba484d1d59a9f9bfd583d47910a
Uthoai Marma <134888799+Uthoai@users.noreply.github.com>
2025-01-12 12:07:55 +0000

    "iteration four"

---
+++ 05_iterations/four.js
@@ -0,0 +1,9 @@
+const myObject = {
+    name: "Uthoai",
+    age: 26,
+    phone: "01300040509",
+    address: "Mirpur Dhaka",
+    occupation: "Android Developer"
+}
+
+
---
+++ 05_iterations/three.js
@@ -0,0 +1,42 @@
+
+
+const arr = [1,2,3,4,5]
+// for (const num of arr) {
+//     console.log(num);
+// }
+
+
+// const greeting = "Hello World"
+// for (const element of greeting) {
+//     console.log(element);
+// }
+
+
+
+//Map
+
+const map = new Map()
+map.set(101, "Uthoai")
+map.set(102, "Pritom")
+map.set(103, "Joy")
+map.set(104, "Mishar")
+
+// console.log(map);
+
+// for (const [key, value] of map) {
+//     console.log(`${key}: ${value}`);
+// }
+
+
+const myObject = {
+    name: "Uthoai",
+    age: 26,
+    phone: "01300040509",
+    address: "Mirpur Dhaka",
+    occupation: "Android Developer"
+}
+// Error
+// for (const [key, value] of myObject) {
+//     console.log(`${key} = ${value}`);
+// }
+
