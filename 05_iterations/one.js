2aa1f53241c8cea59207c393e795d12115a46037
Uthoai Marma <134888799+Uthoai@users.noreply.github.com>
2025-01-12 06:48:56 +0000

    "iteration one"

---
+++ 05_iterations/one.js
@@ -0,0 +1,46 @@
+// +++++++++++++++++ For ++++++++++++++
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`Index: ${i}`);
+// }
+
+
+
+// for (let i = 2; i < 5; i++) {
+//     // Outer Loop
+//     for (let j = 1; j <= 10; j++) {
+//         // Onner Loop
+//         console.log(`${i} * ${j} = ${i*j}`);
+//     }    
+//     console.log();
+// }
+
+
+
+// let superHero = ["Super Man", "Iron Man", "Deadpool"];
+
+// for (let index = 0; index < superHero.length; index++) {
+//     const element = superHero[index];
+//     console.log(element);
+// }
+
+
+
+
+// break and continue
+
+// let arr = new Array();
+// for (let index = 1; index <= 20; index++) {
+//     if (index == 16) {
+//         break;       // continue try
+//     }
+//     if (index >= 6 && index <= 15) {
+//         arr.push(index)
+//     }
+//     console.log(index);
+// }
+
+// console.log(arr);
+
+
+
