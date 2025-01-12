b457b372135551d1b0cf5fa9896741ed98bffdea
Uthoai Marma <134888799+Uthoai@users.noreply.github.com>
2025-01-12 09:41:42 +0000

    "iteration two"

---
+++ 05_iterations/two.js
@@ -0,0 +1,20 @@
+
+
+// let onOperetion = true
+// let i = 0
+
+// while (onOperetion) {
+//     console.log(i);
+//     if (i == 10) {
+//         onOperetion = false
+//     }
+//     i++;
+// }
+
+
+
+// let temp = 30
+// do {
+//     console.log(`Temperature: ${temp}`);
+//     temp--;
+// } while (temp > 15);
