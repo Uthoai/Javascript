de914c91afbcd943a28e45c067176d4540f7d6a9
Uthoai Marma <134888799+Uthoai@users.noreply.github.com>
2025-01-12 14:58:06 +0000

    "iteration four"

--- 05_iterations/four.js
+++ 05_iterations/four.js
@@ -5,5 +5,50 @@ const myObject = {
     address: "Mirpur Dhaka",
     occupation: "Android Developer"
 }
+// for (const key in myObject) {
+//     console.log(`${key} = ${myObject[key]}`);
+// }
 
+
+
+const codingLanguage = ["js","c++","c#","java","kotlin"]
+
+// codingLanguage.forEach( function (item) {
+//     console.log(item);
+// } )
+
+// codingLanguage.forEach( (item)=>{ 
+//     console.log(item); 
+// })
+
+
+// function printMe(item){
+//     console.log(item);
+// }
+// codingLanguage.forEach(printMe)
+
+
+// codingLanguage.forEach( (item, key, array)=>{
+//     console.log(item, key, array);
+// })
+
+
+const user = [
+    {
+        name: "Uthoai",
+        id: 101
+    },
+    {
+        name: "Pritom",
+        id: 102
+    },
+    {
+        name: "Joy",
+        id: 103
+    },
+]
+
+user.forEach( (item)=>{
+    console.log(item.name);
+})
 
